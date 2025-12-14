aff5419139cefed40e0acd1d1a2368c711ccd610
ManjakaMC <manjakaandrianavalona12@gmail.com>
2025-12-14 21:06:03 +0300

    feat:departadd

--- src/Controller/DepartmentController.php
+++ src/Controller/DepartmentController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Department;
+use App\Form\DepartmentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 final class DepartmentController extends AbstractController
 {
@@ -23,4 +27,35 @@ final class DepartmentController extends AbstractController
             'controller_name'=>'DepartmentController'
         ]);
     }
+
+     #[Route('/ajoutDepart' ,name:'app_department_ajout')]
+    public function ajoutDepart(EntityManagerInterface $em):Response
+    {
+        $departements = $em->getRepository(Department::class)->findAll();
+        return $this->render('department/ajoutDepart.html.twig',[
+            'departements' => $departements,
+            'controller_name' =>'DepartmentController'
+        ]);    
+    }
+    #[Route('/formulaireAjoutDepart', name: 'formulaire_departement')]
+    public function formulaireAjoutDepart(Request $request, EntityManagerInterface $em): Response
+    {
+        
+        $departement = new Department();
+
+        $form = $this->createForm(DepartmentType::class, $departement);
+        $form->handleRequest($request);
+
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($departement);
+            $em->flush();
+
+            $this->addFlash('success', 'Département créé avec succès !');
+            return $this->redirectToRoute('formulaire_departement');
+        }
+        return $this->render('department/formulaireAjoutDepart.html.twig', [
+            'form' => $form->createView(), 
+        ]);
+}
 }
